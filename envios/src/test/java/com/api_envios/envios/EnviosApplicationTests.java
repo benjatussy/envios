840d1c5ecf169db8fd5b762092d70d1a7ee7c264
benjatussy <benja.contrerasg@duocuc.cl>
2025-06-04 22:53:24 -0400

    first commit

---
+++ envios/src/test/java/com/api_envios/envios/EnviosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_envios.envios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnviosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
